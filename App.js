58db08b3f9b42619eb23222747a9f5ab94f74181
lakhanimamtagit <lakhani.mamta90@gmail.com>
2024-01-08 21:47:27 +0530

    lect-02

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 // const headingnew = React.createElement("h1",{id: "headingNew"}, "Hello World from App.js file");
 // console.log("headingnew"); // return a object
 // root.render(headingnew);
@@ -50,13 +53,13 @@ React.createElement("div", { id: "child" },
 const parent = React.createElement("div", 
 { id: "parent" }, 
 [React.createElement("div", { id: "child" }, 
-[React.createElement("h1", {}, "I am h1 tag"), React.createElement("h2", {}, "I am h2 tag")]
+[React.createElement("h1", {}, "I am h1 tag"), React.createElement("h1", {}, "Namste React "), React.createElement("h1", {}, "Namste React newwwww "),React.createElement("h2", {}, "I am h2 tag")]
 ),
 React.createElement("div", { id: "child2" }, 
 [React.createElement("h1", {}, "I am h1 tag"), React.createElement("h2", {}, "I am h2 tag")]
 )]
 );
 
-console.log(parent);
+// console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Namaste React
+
+
+# parcel
+- Dev Build
+- Local server
+- HMR Hot Module Replacement
+- File Watching Algorithm  written in C++
+- faster builds by caching
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling (diff bundler for diff type of app and support older version of browser also)
+- Diagnostic
+- Error Handling
+- https 
+- Tree shaking- remove unused code
+- Different dev and prod bundles
+
